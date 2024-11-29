0c7b6ad30892dc711982454881a906e153b595ec
Darya <darya.kononova@list.ru>
2024-11-29 13:45:05 +0300

    change HW_lesson_1

--- lesson1/IfElseStatementTheme.java
+++ lesson1/IfElseStatementTheme.java
@@ -19,7 +19,7 @@ public class IfElseStatementTheme {
         System.out.println();
 
         double height = 1.67;
-        if (height < 1.8 ) {
+        if (height < 1.8) {
             System.out.println("Средний рост");
         } else {
             System.out.println("Высокий рост");
@@ -32,13 +32,10 @@ public class IfElseStatementTheme {
         } else if (nameFirstLetter == 'I') {
             System.out.println("Иванна");
         } else {
-            System.out.println("Имя на букву:" + nameFirstLetter);
+            System.out.println("Имя на букву: " + nameFirstLetter);
         }
 
         System.out.println();
-        System.out.println();
-        System.out.println();
-
 
         System.out.println("2.Поиск большего числа:");
         int number = 2;
@@ -47,14 +44,12 @@ public class IfElseStatementTheme {
         if (number == number1) {
             System.out.println("Числа равны");
         } else if (number1 < number) {
-            System.out.println("Наибольшее число:" + number);
+            System.out.println("Наибольшее число: " + number);
         } else {
-            System.out.println("Наибольшее число:" + number1);
+            System.out.println("Наибольшее число: " + number1);
         }
-        System.out.println();
-        System.out.println();
-        System.out.println();
 
+        System.out.println();
 
         System.out.println("3.Проверка числа:");
         number = 8;
@@ -69,37 +64,35 @@ public class IfElseStatementTheme {
                 System.out.println(number + " " + "отрицательное чётное");
             } else if (number % 2 != 0) {
                 System.out.println(number + " " + "отрицательное нечётное");
+            }
         } else if (number == 0) {
-            System.out.println(number + " " + "равно нулю");
+                System.out.println(number + " " + "равно нулю");
         }
-        }
-        
 
         System.out.println();
-        System.out.println();
-        System.out.println();
-
 
         System.out.println("4. Поиск одинаковых цифр в числах:");
         int a = 123;
         int b = 223;
+        System.out.println("Исходные цифры: " + a + "," + b);
         if (a == b) {
             System.out.println("Числа равны");
-        }
-        int ones1 = a % 10, tens1 = a / 10 % 10, hunds1 = a / 100 % 10;
-        int ones2 = b % 10, tens2 = b / 10 % 10, hunds2 = b / 100 % 10;
-        if (ones1 != ones2 && tens1 != tens2 && hunds1 != hunds2) {
-            System.out.println("Нет совпадающих цифр, все разряды разные");
         } else {
-            System.out.println("Исходные цифры:" + a + "," + b);
+            int ones1 = a % 10;
+            int tens1 = a / 10 % 10;
+            int hunds1 = a / 100 % 10;
+            int ones2 = b % 10;
+            int tens2 = b / 10 % 10;
+            int hunds2 = b / 100 % 10;
+            if (ones1 != ones2 && tens1 != tens2 && hunds1 != hunds2) {
+                System.out.println("Нет совпадающих цифр, все разряды разные");
+            } else {
+                if (ones1 == ones2) System.out.println("Среди разрядов: единицы равны");
+                if (tens1 == tens2) System.out.println("десятки равны");
+                if (hunds1 == hunds2) System.out.println("сотни равны");
+            }
         }
-        if (ones1 == ones2) System.out.println("Среди разрядов: единицы равны");
-        if (tens1 == tens2) System.out.println("десятки равны");
-        if (hunds1 == hunds2) System.out.println("сотни равны");
-        
 
-        System.out.println();
-        System.out.println();
         System.out.println();
 
         System.out.println("5. Определение символа по его коду:");
@@ -113,10 +106,7 @@ public class IfElseStatementTheme {
         } else {
             System.out.println(code + "ни буква ни цифра");
         }
-             
-             
-        System.out.println();
-        System.out.println();
+
         System.out.println();
 
         System.out.println("6.Подсчёт начисленных банком процентов:");
@@ -134,13 +124,11 @@ public class IfElseStatementTheme {
         }
 
         System.out.println();
-        System.out.println();
-        System.out.println();
 
         System.out.println("7.Определение оценки по предметам:");
         float history = 59f;
         float programming = 92f;
-        float gpa = (2+5) / 2f;
+        float gpa = (2 + 5) / 2f;
         float averadgePersent = (history + programming) / 2f;
         if (history <= 60) {
             System.out.println("Оценка по истории-2");
@@ -150,7 +138,7 @@ public class IfElseStatementTheme {
                 System.out.println("Оценка по истории-4");
         } else if (history >= 91) {
                 System.out.println("Оценка по истории-5");
-        } 
+        }
         if (programming <= 60) {
                 System.out.println("Оценка по программированию-2");
         } else if (programming > 60 && programming < 73) {
@@ -164,8 +152,6 @@ public class IfElseStatementTheme {
         System.out.println("Средний процент по предметам -" + averadgePersent);
 
         System.out.println();
-        System.out.println();
-        System.out.println();
 
         System.out.println("8.Расчёт годовой прибыли:");
         float monthlySales = 13025.233f;
@@ -173,14 +159,13 @@ public class IfElseStatementTheme {
         float productionCost = 9001.729f;
         float annualProfit = monthlySales * 12 - (rentPrice * 12 + productionCost * 12);
         if (annualProfit > 0) {
-            System.out.println("Прибыль за год:" + "+" + annualProfit + "руб.");
+            System.out.println("Прибыль за год:" + "+" + annualProfit + " " + "руб.");
         } else if (annualProfit < 0) {
-            System.out.println("Прибыль за год:" + annualProfit + "руб.");
+            System.out.println("Прибыль за год:" + annualProfit + " " + "руб.");
         } else {
-            System.out.println("Прибыль за год:" + annualProfit + "руб.");
+            System.out.println("Прибыль за год:" + annualProfit + " " + "руб.");
         }
     }
-
 }
 
 
