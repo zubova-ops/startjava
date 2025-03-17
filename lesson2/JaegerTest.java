d1547f26b424ba15757456be8f8925d851d4e981
Darya <darya.kononova@list.ru>
2025-03-17 14:52:50 +0300

    Itogovoe

---
+++ lesson2/JaegerTest.java
@@ -0,0 +1,22 @@
+public class JaegerTest {
+    public static void main(String[] args ) {
+        Jaeger jaegerOne = new Jaeger();
+        Jaeger jaegerTwo = new Jaeger("Striker Eureka", "Mark-5", "USA", 250, 3, 6, 8);
+
+        jaegerOne.setModelName("Gipsy Danger");
+        jaegerOne.setMark("Mark-3");
+        jaegerOne.setOrigin("USA");
+        jaegerOne.setHeight(260);
+        jaegerOne.setWeight(2);
+        jaegerOne.setStrength(8);
+        jaegerOne.setArmor(6);
+
+        jaegerOne.sit();
+        jaegerOne.go();
+        jaegerOne.move();
+        jaegerOne.drift();
+
+        System.out.println("name = " + jaegerOne.getModelName());
+        System.out.println("name2 = " + jaegerTwo.getModelName());
+    }
+}
---
+++ lesson2/Player.java
@@ -0,0 +1,23 @@
+public class Player {
+    private String name;
+    private int number;
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    void setNumber(int number) {
+        this.number = number;
+    }
+}
