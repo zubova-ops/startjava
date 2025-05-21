db3cf4a84071490a7b7a195c0b423f518bca060f
Staymix <staszubov199530@gmail.com>
2025-05-21 10:55:54 +0500

    HW_4_array_2

--- src/com/startjava/lesson_4/array/LoadingAnimation.java
+++ src/com/startjava/lesson_4/array/LoadingAnimation.java
@@ -1,6 +1,9 @@
 package com.startjava.lesson_4.array;
 
 public class LoadingAnimation {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) throws InterruptedException {
         char[] spinnerSymbols = {'-', '\\', '|', '/' };
         for (int i = 1; i <= 3; i++) {
@@ -14,9 +17,9 @@ public class LoadingAnimation {
         int min = 0;
         int randomNum = min + (int) (Math.random() * (98 + 1));
         if (randomNum > 70) {
-            System.out.print("Access Granted!");
+            System.out.print(ANSI_RED + "Access Granted!" + ANSI_RESET);
         } else {
-            System.out.print("Access Denied!");
+            System.out.print(ANSI_YELLOW + "Access Denied!" + ANSI_RESET);
 
         }
 
