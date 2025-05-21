9026d92e20454b56eecaffbd2b253c013a7e39d7
Staymix <staszubov199530@gmail.com>
2025-05-21 11:09:12 +0500

    HW_4_array_2

--- src/com/startjava/lesson_4/array/LoadingAnimation.java
+++ src/com/startjava/lesson_4/array/LoadingAnimation.java
@@ -6,22 +6,19 @@ public class LoadingAnimation {
     public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) throws InterruptedException {
         char[] spinnerSymbols = {'-', '\\', '|', '/' };
-        for (int i = 1; i <= 3; i++) {
-            for (int z = 0; z < spinnerSymbols.length; z++) {
-                System.out.print("Haking: " + spinnerSymbols[z]);
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0; k < spinnerSymbols.length; k++) {
+                System.out.print("Haking: " + spinnerSymbols[k]);
                 System.out.print('\r');
                 Thread.sleep(500);
             }
         }
         System.out.print('\r');
-        int min = 0;
-        int randomNum = min + (int) (Math.random() * (98 + 1));
+        int randomNum =  (int) (Math.random() * (98 + 1));
         if (randomNum > 70) {
             System.out.print(ANSI_RED + "Access Granted!" + ANSI_RESET);
         } else {
             System.out.print(ANSI_YELLOW + "Access Denied!" + ANSI_RESET);
-
         }
-
     }
 }
