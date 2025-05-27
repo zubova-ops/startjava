88dc05c6bb6bdeb7fedd9ff98cbce47016088efd
Staymix <staszubov199530@gmail.com>
2025-05-27 13:42:26 +0500

    HW_lesson_5

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,37 +1,58 @@
 --echo Вывод таблицы
-SELECT * FROM jaegers;
+SELECT *
+FROM jaegers
+ORDER BY model_name;
 
 --echo Вывод только неуничтоженных роботов
-SELECT * FROM jaegers WHERE status = 'Active';
+SELECT *
+FROM jaegers
+WHERE status = 'Active'
+ORDER BY model_name;
 
 --echo Вывод только роботов серии Mark-1 и Mark-4
-SELECT * FROM jaegers WHERE mark IN(1,4) ;
+SELECT *
+FROM jaegers
+WHERE mark IN(1,4)
+ORDER BY model_name;
 
 --echo Вывод  всех роботов, кроме Mark-1 и Mark-4,
 -- отсортировав таблицу по убыванию по столбцу mark
-SELECT * FROM jaegers WHERE mark NOT IN(1,4)
+SELECT *
+FROM jaegers
+WHERE mark NOT IN(1,4)
 ORDER BY mark DESC;
 
 --echo-all информация о самых старых роботах
-SELECT * FROM jaegers ORDER BY weight DESC;
+SELECT *
+FROM jaegers
+ORDER BY weight DESC;
 
 --echo Вывод информации из столбцов model_name, mark, launch, kaiju_kill
 -- тех роботов, которые уничтожили больше всех kaiju
-SELECT model_name, mark, launch, kaiju_kill FROM jaegers
+SELECT model_name, mark, launch, kaiju_kill
+FROM jaegers
 ORDER BY kaiju_kill DESC;
 
 --echo средний вес роботов, округлив его до трех знаков после запятой.
-SELECT ROUND(AVG(weight), 3) as avg_weight FROM jaegers;
+SELECT ROUND(AVG(weight), 3) as avg_weight
+FROM jaegers
+ORDER BY model_name;
 
 --echo увеличение  на единицу количество уничтоженных
 -- kaiju у неразрушенных роботов, отображение таблицы
-UPDATE jaegers SET kaiju_kill = kaiju_kill + 1
+UPDATE jaegers
+SET kaiju_kill = kaiju_kill + 1
 WHERE status = 'Active';
-SELECT * FROM jaegers;
+SELECT *
+FROM jaegers
+ORDER BY model_name;
 
 --echo удаление уничтоженных роботов, отображение оставшихся
-DELETE FROM jaegers
+DELETE
+FROM jaegers
 WHERE status = 'Destroyed';
-SELECT * FROM jaegers;
+SELECT *
+FROM jaegers
+ORDER BY model_name;
 
 
